9a53da25714e472815d6b7bf1d1fd5ed67f33f0c
Viktor Farcic <viktor@farcic.com>
2015-01-31 01:23:03 +0100

    Fixed missing username

--- docker/tests/bootstrap.sh
+++ docker/tests/bootstrap.sh
@@ -3,4 +3,4 @@
 sudo mkdir -p /data/.ivy2
 sudo docker build -t vfarcic/books-service-tests docker/tests/.
 sudo docker push vfarcic/books-service-tests
-sudo docker run -t --rm -v $PWD:/source -v /data/.ivy2:/root/.ivy2/cache books-service-tests
+sudo docker run -t --rm -v $PWD:/source -v /data/.ivy2:/root/.ivy2/cache vfarcic/books-service-tests
