fd6f1de7fc472005de846a0203de9417ce402e3a
vfarcic <viktor@farcic.com>
2015-05-19 18:43:47 -0700

    Changed to mongo URI

--- src/main/scala/com/technologyconversations/api/ServiceActor.scala
+++ src/main/scala/com/technologyconversations/api/ServiceActor.scala
@@ -19,10 +19,8 @@ case class Book(_id: Int, title: String, author: String, description: String) {
 
 class ServiceActor extends Actor with ServiceRoute {
 
-  val client = MongoClient(
-    envOrElse("DB_PORT_27017_TCP_ADDR", "localhost"),
-    envOrElse("DB_PORT_27017_TCP_PORT", "27017").toInt
-  )
+  val address = envOrElse("DB_PORT_27017_TCP", "localhost:27017")
+  val client = MongoClient(MongoClientURI(s"mongodb://$address"))
   val db = client(envOrElse("DB_DBNAME", "books"))
   val collection = db(envOrElse("DB_COLLECTION", "books"))
 
