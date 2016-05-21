7d160049da765060bdd9f605f8d170d3fccfe990
vfarcic <viktor@farcic.com>
2016-05-22 00:40:09 +0200

    Added  --force-yes to Ansible in order to avoid the problem with the key signature

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@ echo "Installing Ansible..."
 apt-get install -y software-properties-common
 apt-add-repository ppa:ansible/ansible
 apt-get update
-apt-get install -y ansible
+apt-get install -y --force-yes ansible
