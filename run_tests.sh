cbe4b478566096711e47dfebc21bf5db9d48950e
vfarcic <viktor@farcic.com>
2016-06-15 22:36:02 +0200

    Workaround imitation of a watcher until tests are fixed

--- run_tests.sh
+++ run_tests.sh
@@ -14,6 +14,7 @@ then
   Xvfb :1 -screen 0 1024x768x16 &>/dev/null  &
   echo ">>> NPM is broken (again) so front-end tests are temporarily disabled."
   # gulp watch
+  cat
 elif [ "$TEST_TYPE" = "all" ]
 then
   mongod &
