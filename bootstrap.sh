ad188d1d66213eafdd9305ea4e200ae7861e686b
Lunfu Zhong <zhong.lunfu@gmail.com>
2016-11-15 18:07:31 +0800

    Exec apt commands with sudo.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing Ansible..."
-apt-get install -y software-properties-common
-apt-add-repository ppa:ansible/ansible
-apt-get update
-apt-get install -y --force-yes ansible
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y --force-yes ansible
